b4c76bc6893b31cffe93ad742dbe46cd0065ae3c
RiosGustavo <gustavosuperlango@gmail.com>
2023-07-14 19:08:26 -0500

    implementacion de la seguridad web

--- src/main/java/com/Huertas_agroecologicas/demo/servicios/UsuarioServicio.java
+++ src/main/java/com/Huertas_agroecologicas/demo/servicios/UsuarioServicio.java
@@ -7,8 +7,10 @@ package com.Huertas_agroecologicas.demo.servicios;
 import com.Huertas_agroecologicas.demo.entiddes.Imagen;
 import com.Huertas_agroecologicas.demo.entiddes.Usuario;
 import com.Huertas_agroecologicas.demo.enumeraciones.Rol;
+import com.Huertas_agroecologicas.demo.excepciones.MiException;
 import com.Huertas_agroecologicas.demo.repositorios.UsuarioRepositorio;
 import java.util.Date;
+import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -23,43 +25,72 @@ import org.springframework.web.multipart.MultipartFile;
  * @author User
  */
 @Service
-public class UsuarioServicio implements UserDetailsService{
-    
+public class UsuarioServicio implements UserDetailsService {
+
     @Autowired
     private UsuarioRepositorio usuarioRepositorio;
-    
+
     @Autowired
     private ImagenServicio imagenServicio;
-    
+
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
-    
-     @Transactional
-    public void crearUsuario(MultipartFile archivo, String email, String password, String password2){
-        
+
+    @Transactional
+    public void crearUsuario(MultipartFile archivo, String email, String password, String password2) throws MiException {
+
+        validar(email, password, password2);
+
         Usuario usuario = new Usuario();
-        
+
         usuario.setEmail(email);
         usuario.setPassword(new BCryptPasswordEncoder().encode(password));
         usuario.setFechaAlta(new Date());
         usuario.setRoles(Rol.USER);
         usuario.setAltaBaja(Boolean.TRUE);
-        
+
         Imagen imagen = imagenServicio.guardar(archivo);
-        
+
         usuario.setImagen(imagen);
-        
+
         usuarioRepositorio.save(usuario);
+
+    }
+
+    @Transactional
+    public void ModificarUsuario(MultipartFile archivo, String id, String email, String password, String password2) throws MiException {
+
+        validar(email, password, password2);
         
+        Optional<Usuario> respuesta = usuarioRepositorio.findById(id);
         
+        if (respuesta.isPresent()) {
+            Usuario usuario = respuesta.get();
+
+            usuario.setEmail(email);
+            usuario.setPassword(new BCryptPasswordEncoder().encode(password));
+            String idImagen = null;
+
+            if (usuario.getImagen() != null) {
+                idImagen = usuario.getImagen().getId();
+            }
+
+            Imagen imagen = imagenServicio.actualizar(archivo, idImagen);
+
+            usuario.setImagen(imagen);
+            usuarioRepositorio.save(usuario);
+        }
         
-        
-        
+
     }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
+    private void validar(String email, String password, String password2) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
 }
